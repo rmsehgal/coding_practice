5c60d978b3cac22e6923cebb5b30a4b5cfab53ea
Ray Sehgal <rmsehgal@gmail.com>
2017-01-30 13:00:39 -0500

    adding skeleton of example grading code

---
+++ grade_example.py
@@ -0,0 +1,20 @@
+'''
+Script to grade a completed prompt
+
+Takes an input text file, and runs the results through a set of
+unit tests. These unit test are pulled from a file dynamically
+which is identfied in the header of the input file
+'''
+
+
+
+
+
+if __name__ == "__main__":
+    #run cli arg parser
+
+    #from input text file get name of file for unit tests
+
+    #read in unit test and file
+    
+    #run unit test for file
